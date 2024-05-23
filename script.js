161600515cbb016a4748f50ef639b60ba70519ac
Makha.D <87713231+mbd89@users.noreply.github.com>
2024-05-23 20:56:09 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,122 @@
+const myH1 = document.querySelector('.title')
+const pickSubject = document.querySelector('.choice')
+const mySection = document.querySelector('.welcome')
+const htmlHeader = document.querySelector('.Html')
+const choices = document.querySelectorAll('.subject')
+const choice = document.querySelectorAll('.option')
+const topicIcon = document.querySelectorAll('.subject-icon')
+const submitBtn = document.querySelector('.btn')
+const myAnswer = document.querySelectorAll('.topic')
+const header = document.querySelector('header')
+let count = 0;
+let count2 = 0;
+
+async function populate() {
+  const response = await fetch("data.json")
+  const result = await response.json();
+
+  let questions = (result.quizzes[0].questions)
+  let questionOneOptions = (result.quizzes[0].questions[0].options)
+
+
+  /* for (let i = 0; i<questions.length; i++){
+    myPara.textContent = questions[i]
+  }*/
+
+  /*for (let i = 0; i<questionOneOptions.length; i++){
+    console.log(questionOneOptions[i])
+  }*/
+ header.appendChild(htmlHeader)
+  htmlHeader.classList.remove('hidden')
+  myH1.classList.add('hidden')
+  pickSubject.classList.add('hidden')
+  let question1 = result.quizzes[0].questions[0].question
+  let num = 1;
+  const questionNum = document.createElement('p')
+  questionNum.classList.add('questionNum')
+  questionNum.textContent = `Question ${num} of 10`
+  const myPara = document.createElement('p')
+  mySection.appendChild(questionNum)
+  mySection.appendChild(myPara)
+  myPara.classList.add('questionStyle')
+  myPara.textContent = question1
+  
+  submitBtn.classList.remove('hidden')
+
+  for (let i = 0; i < myAnswer.length; i++) {
+    myAnswer[i].addEventListener('click', () => {
+      choice[i].classList.add('blueBg')
+      myAnswer[i].classList.add('blueBord')
+      count++;
+    })
+  }
+
+
+
+  for (let i = 0; i < topicIcon.length; i++) {
+    topicIcon[i].classList.add('hidden')
+  }
+
+  for (let i = 0; i < choice.length; i++) {
+    choice[i].classList.remove('hidden')
+
+  }
+
+  for (let i = 0; i < choices.length; i++) {
+    choices[i].textContent = questionOneOptions[i]
+  }
+
+  // console.log(result.quizzes[0].questions[0])
+
+  submitBtn.addEventListener('click', () => {
+    count2++;
+    submitBtn.textContent = 'Next Question'
+   
+  })
+  
+}
+let answer;
+populate()
+
+
+function HtmlOptions(questionOptions) {
+  for (let i = 0; i < choices.length; i++) {
+    choices[i].textContent = questionOptions[i]
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function generateHtml(obj) {
+  let questions = obj.quizzes[0].questions
+  let question1 = obj.quizzes[0].questions[0].question
+  let num = 1;
+  const questionNum = document.createElement('p')
+  questionNum.classList.add('choice')
+  questionNum.textContent = `Question ${num} of 10`
+  const myPara = document.createElement('p')
+  myPara.textContent = question1
+
+  for (let i = 0; i < choices.length; i++) {
+    choices[i].textContent = questionOneOptions[i]
+  }
+
+
+}
+
+
+
+
+
